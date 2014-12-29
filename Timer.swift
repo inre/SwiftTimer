8df9f47051a78c5e50f0e4a6ece7e9b004d47601
chelovekov <chelovekov@gmail.com>
2014-12-29 22:12:25 +0300

    NSTimer Swift implementation

---
+++ Timer.swift
@@ -0,0 +1,51 @@
+//  Timer.swift
+//
+//  Copyright (c) 2014 Ivan Chelovekov
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Foundation
+
+@objc class Timer: NSObject {
+    var timer: NSTimer!
+    var handler: (() -> ())!
+    let duration: Double
+    
+    init(duration: Double) {
+        self.duration = duration
+        super.init()
+    }
+    
+    func start(handler: () -> ()) {
+        self.handler = handler
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(duration, target: self, selector: "call", userInfo: nil, repeats: true)
+    }
+    
+    func stop() {
+        self.timer.invalidate()
+    }
+    
+    @objc func call() {
+        self.handler()
+    }
+    
+    deinit {
+        self.timer.invalidate()
+    }
+}
